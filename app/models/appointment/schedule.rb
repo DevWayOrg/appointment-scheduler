e809e8b6087c4cf6afdaa5bd28c184e031c583cc
vinibispo <vini.bispo015@gmail.com>
2024-04-27 22:23:30 -0300

    feat: add appointment info to the database and ensure tests are working properly

--- app/models/appointment/schedule.rb
+++ app/models/appointment/schedule.rb
@@ -1,33 +1,62 @@
-class Appointment::Schedule < Solid::Process
-  input do
-    attribute :name, :string
-    attribute :reason, :string
-    attribute :date, :date
-    attribute :time, :time
+module Appointment
+  class Schedule < Solid::Process
+    input do
+      attribute :name, :string
+      attribute :reason, :string
+      attribute :date, :date
+      attribute :time, :time
 
-    validates :name, :reason, :date, :time, presence: true
-  end
-
-  deps do
-    attribute :today, default: DateTime.now
-  end
-
-  def call(attributes)
-    Given(attributes).and_then(:check_if_appointment_is_more_or_equal_than_now)
-  end
-
-  def check_if_appointment_is_more_or_equal_than_now(date:, time:, **)
-    is_before_today = date.day < deps.today.day && date.month < deps.today.month && date.year < deps.today.year
-    if is_before_today
-      input.errors.add(:date, message: 'must be greater than or equal to today')
-      return Failure(:invalid_input, input:)
+      validates :name, :reason, :date, :time, presence: true
     end
 
-    if date.day == deps.today.day && date.month == deps.today.month && date.year == deps.today.year && (time.hour < deps.today.hour || time.hour == deps.today.hour && time.min < deps.today.minute)
-      input.errors.add(:time, message: 'must be greater than now')
-      return Failure(:invalid_input, input:)
+    deps do
+      attribute :today, default: DateTime.now
+      attribute :repository, default: Repository
     end
 
-    Success(:appointment_verified, appointment: {}, **)
+    def call(attributes)
+      rollback_on_failure {
+        Given(attributes)
+          .and_then(:check_if_appointment_is_more_or_equal_than_now)
+          .and_then(:create_an_appointment)
+      }
+    end
+
+    private
+
+    def check_if_appointment_is_more_or_equal_than_now(date:, time:, **)
+      if before_today?(date:)
+        input.errors.add(:date, message: 'must be greater than or equal to today')
+        return Failure(:invalid_input, input:)
+      end
+
+      if before_or_now?(date:, time:)
+        input.errors.add(:time, message: 'must be greater than now')
+        return Failure(:invalid_input, input:)
+      end
+
+      Continue(date:, time:, **)
+    end
+
+    def before_today?(date:, **)
+      return true if date.day < deps.today.day && date.month <= deps.today.month && date.year <= deps.today.year
+
+      false
+    end
+
+    def before_or_now?(date:, time:)
+      return false if date.day > deps.today.day && date.month >= deps.today.month && date.year >= deps.today.year
+
+      return true if time.hour <= deps.today.hour && time.min <= deps.today.min
+
+      false
+    end
+
+    def create_an_appointment(name:, reason:, time:, date:, **)
+      date_field = DateTime.new(date.year, date.month, date.day, time.hour, time.min)
+      input = { name:, reason:, date: date_field }
+      output = deps.repository.insert(input)
+      Continue(appointment: output)
+    end
   end
 end
